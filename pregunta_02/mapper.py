86ca929ce9ef1d4d335ad48461182b2a65b09cb9
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-08 07:53:42 -0500

    Formato documento

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -3,10 +3,11 @@
 #
 import sys
 
+
 def purpose_amount(x):
     return x[3] + "*" + x[4]
+
 
 for line in sys.stdin:
     result = line.split(',')
     print(purpose_amount(result))
-
--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,10 +1,13 @@
 import sys
-biggest_purposes = {};
+biggest_purposes = {}
+
 
 def set_bigger_purpose(dictionary_purposes, actual_element):
     element_array = actual_element.split("*")
-    dictionary_purposes[element_array[0]] = max(int(dictionary_purposes.get(element_array[0]) or 0),int(element_array[1])) 
+    dictionary_purposes[element_array[0]] = max(
+        int(dictionary_purposes.get(element_array[0]) or 0), int(element_array[1]))
     return dictionary_purposes
+
 
 for line in sys.stdin:
     set_bigger_purpose(biggest_purposes, line)
