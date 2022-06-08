e952dfc2f5b76968d68d83e7e003f2e0f3c693b3
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-08 00:33:34 -0500

    Segundo punto ok
    Se realiza el segundo punto con validaciones

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,62 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import fileinput
+import glob
+import os
+import string 
+from multiprocessing import Pool
+from toolz.functoolz import compose
+from toolz.itertoolz import concat
+
+# -------------------------------- Utils --------------------------------
+def tolower(x):
+    return x.lower()
+
+def remove_punctuation(x):
+    return x.translate(str.maketrans("", "", string.punctuation))
+
+def remove_newline(x):
+    return x.replace("\n", "")
+
+def split_lines(x):
+    return x.split(',')
+
+def create_purpose_amount(x):
+    return x[3] + "*" + x[4]
+# -------------------------------- End Utils --------------------------------
+# ------------------------------ Mapper -------------------------------------
+class Mapper:
+    def __init__(self, input_stream):
+        self.file_path = input_stream
+
+    def load_data(self):
+        def make_iterator_from_single_file():
+            with open(self.file_path, "rt") as file:
+                for line in file:
+                    yield line
+
+        def make_iterator_from_multiple_files():
+            file_path = os.path.join(self.file_path, "*")
+            files = glob.glob(file_path)
+            with fileinput.input(files=files) as file:
+                for line in file:
+                    yield line
+
+        if os.path.isfile(self.file_path):
+            return make_iterator_from_single_file()
+        return make_iterator_from_multiple_files()
+        
+# ------------------------------ END Mapper --------------------------------
+
+compose_pipeline = compose(
+    create_purpose_amount,
+)
+
+with Pool() as pool:
+    mapper = Mapper("credit.csv")
+    result = pool.map(split_lines, mapper.load_data())
+    result = pool.map(compose_pipeline, result)
+
+for line in list(result):
+    print(line)
--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,14 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+elements_reduced = {};
+
+def get_bigger(acc, nxt):
+    nxt_param = nxt.split("*")
+    actual_value = int(acc.get(nxt_param[0]) or 0)
+    acc[nxt_param[0]] = actual_value if actual_value > int(nxt_param[1]) else int(nxt_param[1])
+    return acc
+
+for line in sys.stdin:
+    get_bigger(elements_reduced, line)
+
+for elements_reduced, index in elements_reduced.items():
+    print(elements_reduced + "	" + str(index), end = '\n')
