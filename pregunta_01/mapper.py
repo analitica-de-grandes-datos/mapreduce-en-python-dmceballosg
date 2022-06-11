754f0f078f27cf76021cf314f648dedec7ea8930
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-10 21:37:49 -0500

    Punto 01 ok

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def purpose_amount(x):
+    return x[2]
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(purpose_amount(result))
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+biggest_purposes = {}
+
+def set_bigger_purpose(dictionary_purposes, actual_element):
+    dictionary_purposes[actual_element] = int(dictionary_purposes.get(actual_element) or 0)  +1 
+    return dictionary_purposes
+
+for line in sys.stdin:
+    set_bigger_purpose(biggest_purposes, line.replace("\n", ""))
+
+for purpose, amount in biggest_purposes.items():
+    print(purpose + "	" + str(amount))
