0ddfc0809c91577b6f29323343d9a0a451790ecc
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-11 16:39:23 -0500

    Punto10 ok

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def purpose_amount(x):
+    return  clear_spaces(x[0]), clear_spaces(x[1]).split(",")
+
+for line in sys.stdin:
+    line = line.replace("'","")
+    result = line.split('\t')
+    number, array = purpose_amount(result) 
+    for letter_array in array:
+        print(letter_array + "*" + number)
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+elements = {}
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def set_bigger_smaller_amount(elements, actual_element):
+    element_array = actual_element.split("*")
+    if element_array[0] in elements:
+        elements[element_array[0]] = elements.get(element_array[0]) +  [int(clear_spaces(element_array[1]))]
+    else:
+        elements[element_array[0]] = [int(clear_spaces(element_array[1]))]
+    return elements
+
+for line in sys.stdin:
+    set_bigger_smaller_amount(elements, line)
+
+for element, array in  elements.items():
+    array = ','.join(str(v) for v in sorted(array))
+    print(element + "	" + array)
+    
