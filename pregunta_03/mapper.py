8689659e03263d339a2f6a10599398c19d0893a7
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-10 23:42:23 -0500

    Clear spaces

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -3,8 +3,13 @@
 #
 import sys
 
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
 def purpose_amount(x):
-    return  x[1].replace("\r\n","") +  "*" + x[0]
+    return  clear_spaces(x[1]) +  "*" + clear_spaces(x[0])
 
 for line in sys.stdin:
     result = line.split(',')
