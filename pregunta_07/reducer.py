76d960bd62f201aee150943d26b43b720102911d
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-06-11 16:53:38 -0500

    Punto 7, 9

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,31 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+biggest_purposes = {}
+
+
+def set_bigger_purpose(dictionary_purposes, actual_element):
+    actual_element = actual_element.replace("\n", "")
+    letter = actual_element.split(" ") [0]
+    if  str(dictionary_purposes.get(letter) or "") == "":
+        dictionary_purposes[letter] =    str(dictionary_purposes.get(letter) or "") + actual_element.split(" ") [1]
+    else:
+        dictionary_purposes[letter] =    str(dictionary_purposes.get(letter) or "") +  "*" + actual_element.split(" ") [1]
+    return dictionary_purposes
+
+
+def sortByColumn3(element):
+    return int(element.split(";")[0])
+
+
+for line in sys.stdin:
+    set_bigger_purpose(biggest_purposes, line)
+
+for purpose, values_and_dates in biggest_purposes.items():
+    elements = values_and_dates.split("*")
+    elements.sort(key =sortByColumn3)
+    biggest_purposes[purpose] = elements
+    for element in elements:
+        print(purpose + "   " + element.split(";")[1] + "   " + element.split(";")[0] )
+
--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,30 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+biggest_purposes = []
+count_elements_col_3 = 0
+
+
+def set_bigger_purpose(array_purposes, actual_element):
+    actual_element = actual_element.replace("\n", "")
+    biggest_purposes.append(actual_element)
+    return array_purposes
+
+
+def sortByColumn3(element):
+    return int(element.split(";")[0])
+
+
+for line in sys.stdin:
+    set_bigger_purpose(biggest_purposes, line)
+
+biggest_purposes.sort(key=sortByColumn3)
+
+for purpose in biggest_purposes:
+    count_elements_col_3 = count_elements_col_3 + 1
+    element = purpose.split(";")
+    print(element[1]+ "   " + element[2] + "   " + element [0])
+    if count_elements_col_3 > 5:
+        break
+     
